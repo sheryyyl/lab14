08f7b5bb319a60356aacdb6da469346d932b8a42
Sheryl Condori <condorisheryl@gmail.com>
2024-10-01 21:43:59 -0500

    lab14

--- js/app.js
+++ js/app.js
@@ -20,13 +20,24 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  let convertirValue = JSON.stringify(this.allProducts);  
+  localStorage.setItem('allProducts', convertirValue);  
 }
 
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+  let respuestaProductos = localStorage.getItem('allProducts');  
+  let parseRespuesta = JSON.parse(respuestaProductos);  
+  console.log(parseRespuesta);  
 
-  this.instantiateProducts();
+  if (respuestaProductos) {  
+      this.allProducts = parseRespuesta;  
+  } else {  
+      this.instantiateProducts();  
+  }  
+
+  console.log(this.allProducts[0].timesClicked);   
 
 }
 
--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById('chart');
 
 /* TODO:
  * - Instantiate a new AppState
@@ -10,7 +10,54 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
-function renderChart() {
-}
+
+function renderChart() {  
+    let state = new AppState();  
+    state.loadItems();  
+
+    let productNames = [];  
+    let productClicks = [];  
+    let productViews = [];  
+
+    for (let i = 0; i < state.allProducts.length; i++) {  
+        productNames.push(state.allProducts[i].name);  
+        productClicks.push(state.allProducts[i].timesClicked);  
+        productViews.push(state.allProducts[i].timesShown);  
+    }  
+
+    let chartObj = {  
+        type: 'bar',  
+        data: {  
+            labels: productNames,  
+            datasets: [{  
+                label: '# of votes',  
+                data: productClicks,  
+                borderWidth: 5,  
+                backgroundColor: ['#ff006e'],  
+                borderColor: ['#ff006e']  
+            }, {  
+                label: '# of views',  
+                data: productViews,  
+                borderWidth: 5,  
+                backgroundColor: ['#8388ec'],  
+                borderColor: ['#8388ec']  
+            }]  
+        },  
+        option: {
+            indexAxis: 'y',  
+            scales: {  
+                y: {  
+                    beginAtZero: true,  
+                    ticks: {color: 'black'}  
+                },  
+                x: {  
+                    ticks: { color: 'black' }  
+                }  
+            }  
+        }  
+    };
+
+    new Chart(canvasElem, chartObj);
+}  
 
 renderChart();
